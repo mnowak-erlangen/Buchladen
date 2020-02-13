9f6d4a970a723c5f4d9ebcc1543f1a8b99b97d5a
Max <Maximilian-Ronaldo.Klante@bs-erlangen.de>
2020-02-13 10:17:59 +0100

    Login verbessern

---
+++ admin/login.php
@@ -0,0 +1,43 @@
+<?php 
+session_start();
+$db = mysqli_connect("localhost", "root", "", "buchladen");
+ 
+if(isset($_GET['login'])) {
+    $benutzerid = $_POST['benutzerid'];
+    $passwort = $_POST['passwort'];
+
+    $statement = "SELECT count(*) as 'Eintrag' FROM kunde WHERE benutzerid like '$benutzerid' AND passwort like '$passwort' AND isAdmin = 1;";
+    $result = mysqli_query($db, $statement);
+
+    $eintrag = mysqli_fetch_assoc($result);
+    if ($eintrag['Eintrag'] == 1) {
+        $_SESSION['userid'] = $benutzerid;
+        die('Login erfolgreich. Weiter zum <a href="buchhandlung.php">Kundenbereich</a>.');
+    } else {
+        $errorMessage = "E-Mail oder Passwort ist ungültig<br>";
+    }   
+}
+?>
+<!DOCTYPE html> 
+ <html> 
+    <head>
+        <title>Login</title>    
+    </head> 
+    <body>
+ 
+<?php 
+if(isset($errorMessage)) {
+    echo $errorMessage;
+}
+?>
+        <form action="?login=1" method="post">
+            Benutzername:<br>
+            <input type="text" size="40" maxlength="50" name="benutzerid"><br><br>
+            
+            Dein Passwort:<br>
+            <input type="password" size="40"  maxlength="250" name="passwort"><br>
+            
+            <input type="submit" value="Abschicken">
+        </form> 
+    </body>
+</html>
--- index.php
+++ index.php
@@ -1,65 +1,12 @@
-<?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-?>
-
-
-<html lang="de">
-	<head>
-		<!-- Required meta tags -->
-		<meta charset="utf-8">
-        <!-- turnjs-->
-		<script type="text/javascript" src="turnjs4/extras/jquery.min.1.7.js"></script>
-		<script type="text/javascript" src="turnjs4/extras/modernizr.2.5.3.min.js"></script>
-
-		<title>Buchladen</title>
-	</head>
-	<body>
-		<!-- Header -->
-		<div>
-			<h2 class="card-title" style="text-align: center; color: black">Buchladen</h2>
-		</div>
-        
-    <div class="flipbook-viewport">
-        <div class="container">
-            <div class="flipbook">
-                <div style="background-image:url(turnjs4/samples/basic/pages/1.jpg)"></div>
-                <div style="background-image:url(turnjs4/samples/basic/pages/2.jpg)"></div>
-                <div style="background-image:url(turnjs4/samples/basic/pages/3.jpg)"></div>
-                <div style="background-image:url(turnjs4/samples/basic/pages/4.jpg)"></div>
-                <div style="background-image:url(turnjs4/samples/basic/pages/5.jpg)"></div>
-                <div style="background-image:url(turnjs4/samples/basic/pages/6.jpg)"></div>
-                <div style="background-image:url(turnjs4/samples/basic/pages/7.jpg)"></div>
-                <div style="background-image:url(turnjs4/samples/basic/pages/8.jpg)"></div>
-                <div style="background-image:url(turnjs4/samples/basic/pages/9.jpg)"></div>
-                <div style="background-image:url(turnjs4/samples/basic/pages/10.jpg)"></div>
-                <div style="background-image:url(turnjs4/samples/basic/pages/11.jpg)"></div>
-                <div style="background-image:url(turnjs4/samples/basic/pages/12.jpg)"></div>
-            </div>
-        </div>
-    </div>
-
-<script type="text/javascript">
-
-function loadApp() {
-	$('.flipbook').turn({
-			width:922,
-			height:600,
-			elevation: 50,
-			gradients: true,
-			autoCenter: true
-	});
-}
-
-yepnope({
-	test : Modernizr.csstransforms,
-	yep: ['turnjs4/lib/turn.js'],
-	nope: ['turnjs4/lib/turn.html4.min.js'],
-	both: ['turnjs4/samples/basic/css/basic.css'],
-	complete: loadApp
-});
-
-</script>
-	</body>
+<!DOCTYPE html> 
+ <html> 
+    <head>
+        <title>Login</title>    
+    </head> 
+    <body>
+		<h1>Buchverleih GmbH</h1>
+		<p><a href="kunde/login.php">Einloggen</a><p>
+		<p><a href="kunde/registrieren.php">Registrieren</a><p>
+		<p><a href="admin/login.php">Als Admin einloggen</a><p>
+    </body>
 </html>
